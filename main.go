32fc7913ac1dfdb97ac6c28b28966de7877c0eb8
Brayden Di Luca <bray.diluca@estimateone.com>
2024-02-29 13:45:45 +1100

    Add endpoint to call BuildPlaylistHandler

--- main.go
+++ main.go
@@ -12,6 +12,21 @@ func main() {
         return c.SendString(handlers.HelloWorldHandler())
     })
 
+	app.Get("/top-songs", func(c *fiber.Ctx) error {
+		date := c.Query("date")
+
+		if (date == "") {
+			return fiber.NewError(fiber.ErrBadRequest.Code, "Missing date parameter")
+		}
+		playlist, err := handlers.BuildPlaylistHandler(date);
+
+		if err != nil {
+			return fiber.NewError(fiber.ErrBadRequest.Code, err.Error())
+		}
+
+		return c.SendString(playlist)
+	})
+
 	app.Get("/:name", func(c *fiber.Ctx) error {
 		return c.SendString(handlers.HelloToHandler(c.Params("name")))
 	})
