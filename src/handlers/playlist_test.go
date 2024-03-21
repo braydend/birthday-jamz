13b1cddee7c851bcfac3e93aa0adea967504558c
Brayden Di Luca <bray.diluca@estimateone.com>
2024-03-21 13:25:55 +1100

    Fix failed tests

--- src/handlers/playlist_test.go
+++ src/handlers/playlist_test.go
@@ -1,15 +1,17 @@
 package handlers_test
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/braydend/birthday-jamz/src/api"
 	"github.com/braydend/birthday-jamz/src/handlers"
 )
 
 func TestPlaylistHandlerTakesValidDate (t *testing.T) {
 	result, err := handlers.BuildPlaylistHandler("1989-01-19");
 
-	if (result != "I would build a date starting on 1989-01-19") {
+	if (reflect.DeepEqual(result, handlers.PlaylistResponse{Date: "1989-01-19", Songs: []api.Song{}})) {
 		t.Logf(err.Error())
 		t.Errorf("Handler returned wrong value");
 	}
@@ -19,7 +21,7 @@ func TestPlaylistHandlerFailsOnWrongDate (t *testing.T) {
 	result, err := handlers.BuildPlaylistHandler("1989-19-01");
 
 	if (err == nil) {
-		t.Logf(result)
+		t.Logf("%v", result)
 		t.Errorf("Handler expected error but none was provided");
 	}
 }
